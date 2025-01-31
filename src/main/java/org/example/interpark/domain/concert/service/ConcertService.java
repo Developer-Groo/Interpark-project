36b41274680fb92f9a83b6acf14280f076e06d85
Groo <woohyeon3382@naver.com>
2025-01-31 17:28:27 +0900

    feat: ConcertService 구현

--- src/main/java/org/example/interpark/domain/concert/service/ConcertService.java
+++ src/main/java/org/example/interpark/domain/concert/service/ConcertService.java
@@ -1,9 +1,27 @@
 package org.example.interpark.domain.concert.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.interpark.domain.concert.dto.response.ConcertSearchResponseDto;
+import org.example.interpark.domain.concert.repository.ConcertRepositoryV1;
+import org.example.interpark.util.PageQuery;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ConcertService {
+
+    private final ConcertRepositoryV1 concertRepositoryV1;
+
+    public org.example.interpark.util.Page<ConcertSearchResponseDto> searchConcerts(String keyword,
+        PageQuery pageQuery) {
+        Page<ConcertSearchResponseDto> concertList = concertRepositoryV1.searchConcerts(keyword,
+                pageQuery.toPageable())
+            .map(concert -> new ConcertSearchResponseDto(concert.getId(), concert.getName(),
+                concert.getAmount()));
+
+        return org.example.interpark.util.Page.from(concertList);
+    }
 }
