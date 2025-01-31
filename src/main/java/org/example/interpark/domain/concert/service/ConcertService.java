85b6e9d50dac413233638be67ea136f8f4ce9ae0
Groo <woohyeon3382@naver.com>
2025-01-31 21:01:29 +0900

    refactor: keyword 저장 메서드 추가
    searchKeywordService.saveSearchKeyword(keyword)

--- src/main/java/org/example/interpark/domain/concert/service/ConcertService.java
+++ src/main/java/org/example/interpark/domain/concert/service/ConcertService.java
@@ -3,6 +3,7 @@ package org.example.interpark.domain.concert.service;
 import lombok.RequiredArgsConstructor;
 import org.example.interpark.domain.concert.dto.response.ConcertSearchResponseDto;
 import org.example.interpark.domain.concert.repository.ConcertRepositoryV1;
+import org.example.interpark.domain.search.service.SearchKeywordService;
 import org.example.interpark.util.PageQuery;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
@@ -11,13 +12,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class ConcertService {
 
     private final ConcertRepositoryV1 concertRepositoryV1;
+    private final SearchKeywordService searchKeywordService;
 
     public org.example.interpark.util.Page<ConcertSearchResponseDto> searchConcerts(String keyword,
         PageQuery pageQuery) {
+        searchKeywordService.saveSearchKeyword(keyword);
+
         Page<ConcertSearchResponseDto> concertList = concertRepositoryV1.searchConcerts(keyword,
                 pageQuery.toPageable())
             .map(concert -> new ConcertSearchResponseDto(concert.getId(), concert.getName(),
@@ -27,7 +31,10 @@ public class ConcertService {
     }
 
     @Cacheable(value = "concerts", key = "#keyword")
-    public org.example.interpark.util.Page<ConcertSearchResponseDto> searchConcertsByCache(String keyword, PageQuery pageQuery) {
+    public org.example.interpark.util.Page<ConcertSearchResponseDto> searchConcertsByCache(
+        String keyword, PageQuery pageQuery) {
+        searchKeywordService.saveSearchKeyword(keyword);
+
         Page<ConcertSearchResponseDto> concertList = concertRepositoryV1.searchConcerts(keyword,
                 pageQuery.toPageable())
             .map(concert -> new ConcertSearchResponseDto(concert.getId(), concert.getName(),
