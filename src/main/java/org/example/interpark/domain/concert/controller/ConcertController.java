f6023ce5350334acc2274a8c666aa1e5911ae39b
Groo <woohyeon3382@naver.com>
2025-01-31 14:54:47 +0900

    feat: controller 구현

--- src/main/java/org/example/interpark/domain/concert/controller/ConcertController.java
+++ src/main/java/org/example/interpark/domain/concert/controller/ConcertController.java
@@ -1,9 +1,23 @@
 package org.example.interpark.domain.concert.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ConcertController {
+
+    @GetMapping("/v1/concerts")
+    public ResponseEntity<String> getConcerts() {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/v2/concerts")
+    public ResponseEntity<String> getConcertsByCache() {
+        return ResponseEntity.ok("");
+    }
 }
