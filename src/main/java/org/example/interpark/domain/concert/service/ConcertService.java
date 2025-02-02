72335d1272eebf73df03a74f39460c368fe2dcba
Groo <woohyeon3382@naver.com>
2025-02-02 19:36:00 +0900

    feat: 중복 로직 제거 및 검색 서비스 레이어 분리

--- src/main/java/org/example/interpark/domain/concert/service/ConcertService.java
+++ src/main/java/org/example/interpark/domain/concert/service/ConcertService.java
@@ -2,8 +2,7 @@ package org.example.interpark.domain.concert.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.interpark.domain.concert.dto.response.ConcertSearchResponseDto;
-import org.example.interpark.domain.concert.repository.ConcertRepositoryV1;
-import org.example.interpark.domain.search.service.SearchKeywordService;
+import org.example.interpark.domain.concert.repository.ConcertQueryRepository;
 import org.example.interpark.util.PageQuery;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
@@ -12,33 +11,31 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class ConcertService {
 
-    private final ConcertRepositoryV1 concertRepositoryV1;
-    private final SearchKeywordService searchKeywordService;
-    private final ConcertPopularSearchService concertPopularSearchService;
+    private final ConcertQueryRepository concertQueryRepository;
 
+    /**
+     * 검색어 Cache 적용 X
+     */
     public org.example.interpark.util.Page<ConcertSearchResponseDto> searchConcerts(String keyword,
         PageQuery pageQuery) {
-        searchKeywordService.saveSearchKeyword(keyword);
-
-        Page<ConcertSearchResponseDto> concertList = concertRepositoryV1.searchConcerts(keyword,
-                pageQuery.toPageable())
-            .map(concert -> new ConcertSearchResponseDto(concert.getId(), concert.getName(),
-                concert.getTotalAmount(), concert.getAvailableAmount()));
-
-        return org.example.interpark.util.Page.from(concertList);
+        return getConcerts(keyword, pageQuery);
     }
 
+    /**
+     * 검색어 Local Memory Cache 적용
+     */
     @Cacheable(value = "concerts", key = "#keyword")
     public org.example.interpark.util.Page<ConcertSearchResponseDto> searchConcertsByCache(
         String keyword, PageQuery pageQuery) {
-        searchKeywordService.saveSearchKeyword(keyword);
+        return getConcerts(keyword, pageQuery);
+    }
 
-        concertPopularSearchService.incrementSearchCount(keyword);
-
-        Page<ConcertSearchResponseDto> concertList = concertRepositoryV1.searchConcerts(keyword,
+    private org.example.interpark.util.Page<ConcertSearchResponseDto> getConcerts(String keyword,
+        PageQuery pageQuery) {
+        Page<ConcertSearchResponseDto> concertList = concertQueryRepository.searchConcerts(keyword,
                 pageQuery.toPageable())
             .map(concert -> new ConcertSearchResponseDto(concert.getId(), concert.getName(),
                 concert.getTotalAmount(), concert.getAvailableAmount()));
