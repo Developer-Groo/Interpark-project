107c6540ebe4a6aa277a1036fa51bd79dcc52b7d
Groo <woohyeon3382@naver.com>
2025-02-02 19:36:36 +0900

    refactor: 주석 추가 및 검색 서비스 레이어 추가

--- src/main/java/org/example/interpark/domain/concert/controller/ConcertController.java
+++ src/main/java/org/example/interpark/domain/concert/controller/ConcertController.java
@@ -1,11 +1,11 @@
 package org.example.interpark.domain.concert.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.interpark.domain.concert.dto.response.ConcertSearchResponseDto;
-import org.example.interpark.domain.concert.service.ConcertPopularSearchService;
 import org.example.interpark.domain.concert.service.ConcertService;
+import org.example.interpark.domain.search.service.ConcertPopularSearchService;
+import org.example.interpark.domain.search.service.SearchKeywordService;
 import org.example.interpark.util.Page;
 import org.example.interpark.util.PageQuery;
 import org.springframework.http.ResponseEntity;
@@ -20,24 +20,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConcertController {
 
     private final ConcertService concertService;
+    private final SearchKeywordService searchKeywordService;
     private final ConcertPopularSearchService concertPopularSearchService;
 
+    /**
+     * Cache 를 적용하지 않은 API
+     */
     @GetMapping("/v1/concerts")
     public ResponseEntity<Page<ConcertSearchResponseDto>> getConcerts(@RequestParam(required = false) String keyword,
         PageQuery pageQuery) {
+        searchKeywordService.saveSearchKeyword(keyword);
         return ResponseEntity.status(HttpServletResponse.SC_OK)
             .body(concertService.searchConcerts(keyword, pageQuery));
     }
 
+    /**
+     * Local Memory Cache 를 적용한 API
+     */
     @GetMapping("/v2/concerts")
     public ResponseEntity<Page<ConcertSearchResponseDto>> getConcertsByCache(@RequestParam(required = false) String keyword,
         PageQuery pageQuery) {
-        return ResponseEntity.ok(concertService.searchConcertsByCache(keyword, pageQuery));
+        searchKeywordService.saveSearchKeyword(keyword);
+        return ResponseEntity.status(HttpServletResponse.SC_OK)
+            .body(concertService.searchConcertsByCache(keyword, pageQuery));
     }
 
-    @GetMapping("/v2/concerts/popular")
-    public ResponseEntity<List<String>> getPopularKeywords() {
-        List<String> topSearchKeywords = concertPopularSearchService.getTopSearchKeywords();
-        return ResponseEntity.ok(topSearchKeywords);
+    /**
+     * Redis Cache 를 적용한 API
+     */
+    @GetMapping("/v2/redis/concerts")
+    public ResponseEntity<Page<ConcertSearchResponseDto>> getConcertsByRedis(@RequestParam(required = false) String keyword,
+        PageQuery pageQuery) {
+        concertPopularSearchService.incrementSearchCount(keyword);
+        return ResponseEntity.status(HttpServletResponse.SC_OK)
+            .body(concertService.searchConcerts(keyword, pageQuery));
     }
 }
