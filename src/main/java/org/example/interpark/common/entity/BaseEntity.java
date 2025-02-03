97691f2d54c8f9b397ec3c26850d9cb2bcab1aef
dllll2 <wlsdud8658@gmail.com>
2025-02-03 14:46:01 +0900

    refector: BaseEntity 수정

--- src/main/java/org/example/interpark/common/entity/BaseEntity.java
+++ src/main/java/org/example/interpark/common/entity/BaseEntity.java
@@ -3,7 +3,9 @@ package org.example.interpark.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
