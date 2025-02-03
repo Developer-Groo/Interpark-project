97aaafe42fccfcf94ed6dae672ee0739a7b28b0b
Groo <woohyeon3382@naver.com>
2025-02-03 18:30:36 +0900

    feat: log 추가

--- src/main/java/org/example/interpark/domain/search/service/SearchKeywordService.java
+++ src/main/java/org/example/interpark/domain/search/service/SearchKeywordService.java
@@ -1,12 +1,14 @@
 package org.example.interpark.domain.search.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.interpark.domain.search.dto.response.SearchKeywordResponseDto;
 import org.example.interpark.domain.search.entity.SearchKeyword;
 import org.example.interpark.domain.search.repository.SearchKeywordRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,17 +21,18 @@ public class SearchKeywordService {
         searchKeywordRepository.findByKeyword(keyword).ifPresentOrElse(
             searchKeyword -> {
                 searchKeyword.incrementCount();
-                searchKeywordRepository.save(searchKeyword);
+                log.info("인기 검색어 카운트 증가: {}", searchKeyword);
             },
             () -> {
                 SearchKeyword searchKeyword = new SearchKeyword(keyword, 1);
                 searchKeywordRepository.save(searchKeyword);
+                log.info("새로운 검색어 카운트 증가: {}", searchKeyword);
             }
         );
     }
 
     public SearchKeywordResponseDto getTopSearchKeywords() {
-        return SearchKeywordResponseDto.toDto(searchKeywordRepository.findTop10ByOrderByCountDesc());
+        return SearchKeywordResponseDto.from(searchKeywordRepository.findTop10ByOrderByCountDesc());
     }
 
 }
