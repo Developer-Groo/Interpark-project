a7e958d93e19309c88a83707d12c7b6dfda32c05
Groo <woohyeon3382@naver.com>
2025-02-03 17:19:36 +0900

    refactor: 주석 수정

--- src/main/java/org/example/interpark/config/RedisConfig.java
+++ src/main/java/org/example/interpark/config/RedisConfig.java
@@ -24,51 +24,50 @@ public class RedisConfig {
     public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Integer> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        // Key 는 String, Value 는 JSON 형식으로 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
+        return template;
+    }
 
-    // Key는 String, Value는 JSON 형식으로 직렬화
-    template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(new GenericToStringSerializer(Integer.class));
+    @Bean
+    public RedisTemplate<String, String> redisTemplateV2(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());  // 키를 String 으로 저장
+        template.setValueSerializer(new StringRedisSerializer()); // 값을 String 으로 저장
+        return template;
+    }
 
-    return template;
-  }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplateV3(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer()); // JSON 직렬화 적용
+        template.afterPropertiesSet();
+        return template;
+    }
 
-  @Bean
-  public RedisTemplate<String, String> redisTemplateV2(RedisConnectionFactory connectionFactory) {
-    RedisTemplate<String, String> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
-    template.setKeySerializer(new StringRedisSerializer());  // 키를 String으로 저장
-    template.setValueSerializer(new StringRedisSerializer()); // 값을 String으로 저장
-    return template;
-  }
+    @Bean
+    public HashOperations<String, String, ConcertSearchResponseDto> hashOperations(
+        RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
 
-  @Bean
-  public RedisTemplate<String, Object> redisTemplateV3(RedisConnectionFactory connectionFactory) {
-    RedisTemplate<String, Object> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10)) // 캐시 TTL 설정 (10분)
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                new StringRedisSerializer())) // key 는 String 으로 직렬화
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer())); // Value 는 JSON 으로 직렬화
 
-    template.setKeySerializer(new StringRedisSerializer());
-    template.setHashKeySerializer(new StringRedisSerializer());
-    template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer()); // JSON 직렬화 적용
-
-    template.afterPropertiesSet();
-    return template;
-  }
-
-  @Bean
-  public HashOperations<String, String, ConcertSearchResponseDto> hashOperations(RedisTemplate<String, Object> redisTemplate) {
-    return redisTemplate.opsForHash();
-  }
-
-  @Bean
-  public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
-    RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-        .entryTtl(Duration.ofMinutes(10)) // 캐시 TTL 설정 (10분)
-        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())) // key는 String으로 직렬화
-        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())); // Value는 JSON으로 직렬화
-
-    return RedisCacheManager.builder(redisConnectionFactory)
-        .cacheDefaults(cacheConfig)
-        .build();
-  }
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(cacheConfig)
+            .build();
+    }
 
 }
