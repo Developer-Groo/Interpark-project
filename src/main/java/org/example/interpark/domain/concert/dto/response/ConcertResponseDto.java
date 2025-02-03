2ae7935f146ff5c02f76a011d476d7894d7de11c
Groo <woohyeon3382@naver.com>
2025-02-03 18:24:10 +0900

    refactor: 띄어쓰기 수정

--- src/main/java/org/example/interpark/domain/concert/dto/response/ConcertResponseDto.java
+++ src/main/java/org/example/interpark/domain/concert/dto/response/ConcertResponseDto.java
@@ -2,7 +2,7 @@ package org.example.interpark.domain.concert.dto.response;
 
 import org.example.interpark.domain.concert.entity.Concert;
 
-public record ConcertResponseDto (int id, String name, int totalAmount, int availableAmount){
+public record ConcertResponseDto(int id, String name, int totalAmount, int availableAmount) {
 
   public static ConcertResponseDto from(Concert concert) {
     return new ConcertResponseDto((concert.getId()), concert.getName(), concert.getTotalAmount(), concert.getAvailableAmount());
