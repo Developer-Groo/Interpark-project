c5ce565b3fa9e942f31a4e166aa9e8b41d6b8ffb
Groo <woohyeon3382@naver.com>
2025-02-03 14:37:26 +0900

    feat: redis 적용 api 추가

--- src/main/java/org/example/interpark/domain/search/controller/SearchKeywordController.java
+++ src/main/java/org/example/interpark/domain/search/controller/SearchKeywordController.java
@@ -3,21 +3,31 @@ package org.example.interpark.domain.search.controller;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.interpark.domain.search.service.RedisSearchKeywordService;
 import org.example.interpark.domain.search.dto.response.SearchKeywordResponseDto;
 import org.example.interpark.domain.search.service.SearchKeywordService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class SearchKeywordController {
 
     private final SearchKeywordService searchKeywordService;
+    private final RedisSearchKeywordService redisSearchKeywordService;
 
-    @GetMapping("/api/v1/keywords/popular")
-    public ResponseEntity<SearchKeywordResponseDto> getTopSearchKeywords() {
+    @GetMapping("/v1/keywords/popular")
+    public ResponseEntity<SearchKeywordResponseDto> getPopularKeywords() {
         return ResponseEntity.status(HttpServletResponse.SC_OK)
             .body(searchKeywordService.getTopSearchKeywords());
+    }
+
+    @GetMapping("/v2/keywords/popular")
+    public ResponseEntity<List<String>> getPopularKeywordsByRedis() {
+        return ResponseEntity.status(HttpServletResponse.SC_OK)
+            .body(redisSearchKeywordService.getTopSearchKeywords());
     }
 }
