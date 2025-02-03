22d176250882d0d37d3653a22eac8676a8296c63
Groo <woohyeon3382@naver.com>
2025-02-03 14:33:45 +0900

    refactor: 네이밍 변경

---
+++ src/main/java/org/example/interpark/domain/search/service/RedisSearchKeywordService.java
@@ -0,0 +1,31 @@
+package org.example.interpark.domain.search.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RedisSearchKeywordService {
+
+    private final RedisTemplate<String, String> redisTemplateV2;
+    private static final String POPULAR_SEARCH_KEY = "popular_keywords"; // ZSet Key
+
+    public void incrementSearchCount(String keyword) {
+        log.info("인기 검색어 증가: {}", keyword);
+        redisTemplateV2.opsForZSet().incrementScore(POPULAR_SEARCH_KEY, keyword, 1);
+    }
+
+    public List<String> getTopSearchKeywords() {
+        log.info("인기 검색어 조회 요청");
+        Set<String> reverseRange = redisTemplateV2.opsForZSet()
+            .reverseRange(POPULAR_SEARCH_KEY, 0, 9);
+        return new ArrayList<>(reverseRange != null ? reverseRange : Collections.emptySet());
+    }
+}
