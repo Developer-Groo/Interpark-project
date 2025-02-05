e5da7626ff50d87586d0b6257aab9492fad63e35
Groo <woohyeon3382@naver.com>
2025-02-05 16:56:02 +0900

    feat: 캐시 스케줄링 구현

---
+++ src/main/java/org/example/interpark/domain/concert/service/ConcertCacheScheduler.java
@@ -0,0 +1,44 @@
+package org.example.interpark.domain.concert.service;
+
+import java.util.HashSet;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ConcertCacheScheduler {
+
+    private final RedisTemplate<String, String> redisTemplateV2;
+
+    //   @Scheduled(cron = "0 */1 * * * *")
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void clearCacheAtMidnight() {
+        HashSet<String> keysToDelete = new HashSet<>();
+
+        RedisConnection connection = Objects.requireNonNull(redisTemplateV2.getConnectionFactory())
+            .getConnection();
+
+        ScanOptions options = ScanOptions.scanOptions()
+            .match("concerts*")
+            .count(100)
+            .build();
+
+        connection.keyCommands().scan(options).forEachRemaining(key ->
+            keysToDelete.add(new String(key))
+        );
+
+        log.info("삭제할 키 목록 {}", keysToDelete);
+
+        if (!keysToDelete.isEmpty()) {
+            redisTemplateV2.delete(keysToDelete);
+            log.info("모든 검색 캐시 데이터 삭제");
+        }
+    }
+}
