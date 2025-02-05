467aa28a6dad6158dd9fc9e9eb2361f8b61ce92b
Groo <woohyeon3382@naver.com>
2025-02-05 17:02:34 +0900

    feat: @EnableScheduling 추가

--- src/main/java/org/example/interpark/InterparkApplication.java
+++ src/main/java/org/example/interpark/InterparkApplication.java
@@ -3,9 +3,11 @@ package org.example.interpark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class InterparkApplication {
 
     public static void main(String[] args) {
