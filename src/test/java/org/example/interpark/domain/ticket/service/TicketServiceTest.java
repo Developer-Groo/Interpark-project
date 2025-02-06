70b158ba9036678fa1b54853ef1bd21b6ba087f5
heehyun0221 <psr4580@gmail.com>
2025-02-06 18:56:04 +0900

    ✅ test : 테스트 코드 수정

--- src/test/java/org/example/interpark/domain/ticket/service/TicketServiceTest.java
+++ src/test/java/org/example/interpark/domain/ticket/service/TicketServiceTest.java
@@ -2,6 +2,8 @@ package org.example.interpark.domain.ticket.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -10,18 +12,23 @@ import java.util.concurrent.TimeUnit;
 import org.example.interpark.domain.concert.entity.Concert;
 import org.example.interpark.domain.concert.repository.ConcertRepository;
 import org.example.interpark.domain.ticket.dto.TicketRequestDto;
+import org.example.interpark.domain.ticket.entity.Ticket;
 import org.example.interpark.domain.ticket.repository.TicketRepository;
 import org.example.interpark.domain.user.entity.User;
 import org.example.interpark.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class TicketServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(TicketServiceTest.class);
 
     @Autowired
     TicketService ticketService;
@@ -42,7 +49,7 @@ class TicketServiceTest {
 
     @BeforeEach
     void setup() {
-        concert = new Concert("콘서트", 2);
+        concert = new Concert("콘서트", 1000);
         concertRepository.save(concert);
         user = new User("gege", "1234", "gege@naver.com");
         userRepository.save(user);
@@ -59,16 +66,14 @@ class TicketServiceTest {
     @Test
     void 티켓생성() throws InterruptedException {
         ExecutorService executorService = new ThreadPoolExecutor(20, 20, 60L, TimeUnit.SECONDS,
-            new LinkedBlockingQueue<>(20));
+            new LinkedBlockingQueue<>(20000));
 
-        CountDownLatch latch = new CountDownLatch(5);
+        CountDownLatch latch = new CountDownLatch(1500);
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <=1500; i++) {
             executorService.execute(() -> {
                 try {
                     ticketService.create(new TicketRequestDto(user.getId(), concert.getId()));
-                    concert = concertRepository.findById(concert.getId())
-                        .orElseThrow(() -> new RuntimeException("일단 봅시다."));
                 } catch (Exception e) {
                     System.out.println(e);
                 } finally {
@@ -80,7 +85,20 @@ class TicketServiceTest {
         latch.await();
         executorService.shutdown();
 
-        Integer availableTicket = concert.getAvailableAmount();
-        assertEquals(0, availableTicket);
+        Concert updatedConcert = concertRepository.findById(concert.getId())
+            .orElseThrow(() -> new RuntimeException("Concert not found"));
+        List<Ticket> tickets = ticketRepository.findAll();
+        Ticket firstTicket = ticketRepository.findById(1).orElseThrow();
+        Ticket endTicket = ticketRepository.findById(1000).orElseThrow();
+        Duration duration = Duration.between(firstTicket.getCreatedAt(),endTicket.getCreatedAt());
+        log.info("---------------------------------------------");
+        log.info("총 티켓 수 : " + updatedConcert.getTotalAmount());
+        log.info("남은 티켓 수 : " + updatedConcert.getAvailableAmount());
+        log.info("총 발급된 티켓 수 : " + tickets.size());
+        log.info("티켓 소진까지 걸린 시간: {}초", String.format("%.2f", duration.toMillis()/1000.0));
+        log.info("---------------------------------------------");
+
+        assertEquals(1000, tickets.size());
+        assertEquals(0, updatedConcert.getAvailableAmount());
     }
 }
